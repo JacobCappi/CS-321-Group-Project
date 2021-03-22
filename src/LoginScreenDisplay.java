9b86cb13c45a2736092ee6ecdaf746c3d2138e01
gdors <gcd0003@uah.edu>
2021-03-22 09:35:48 -0500

    Testing...

--- src/LoginScreenDisplay.java
+++ src/LoginScreenDisplay.java
@@ -3,30 +3,50 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class LoginScreenDisplay  extends JPanel{
     private JPanel loginPanel;
     private JLabel WelcomeLabel;
     private JTextField usernameTextField;
     private JLabel usernameLabel;
-    private JTextField textField1;
+    private JTextField passwordTextField;
     private JLabel passwordLabel;
     private JButton loginButton;
     private JLabel loginLabel;
     private JLabel registerLabel;
     private JButton registerButton;
     final ArrayList<ChangeListener> listeners = new ArrayList<>();
+    JSONParser parser = new JSONParser();
 
-    public  LoginScreenDisplay(){
+
+
+    public  LoginScreenDisplay() throws FileNotFoundException {;
 
 
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ChangeEvent event = new ChangeEvent(this);
-                for (ChangeListener listener: listeners)
-                    listener.stateChanged(event);
+                try {
+                    Object obj = parser.parse(new FileReader("C:/Users/gdors/Documents/test.json"));
+
+                    JSONObject jsonObject = (JSONObject) obj;
+                    if (usernameTextField.getText().equals((String) jsonObject.get("username"))) {
+                        ChangeEvent event = new ChangeEvent(this);
+                        for (ChangeListener listener : listeners)
+                            listener.stateChanged(event);
+                    }
+
+
+                } catch (ParseException | IOException e1) {
+                    e1.printStackTrace();
+                }
 
             }
         });
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class Main {
 
 
-    public static  void main ( String []args ){
+    public static  void main ( String []args ) throws FileNotFoundException {
         JFrame testFrame = new JFrame();
+
         testFrame.setSize(500,500);
         testFrame.setLocationRelativeTo(null);
         final LoginScreenDisplay userLogin = new LoginScreenDisplay();
