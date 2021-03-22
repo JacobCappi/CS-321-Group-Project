49f0608b6bcc33302a8a683f4cb174f9efd59c00
gdors <gcd0003@uah.edu>
2021-03-22 09:37:36 -0500

    Testing...

---
+++ src/cardTestDisplay.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import java.util.ArrayList;
+
+public class cardTestDisplay extends JPanel {
+    private JPanel jklsdhgisdhgsfdkhjgsfg;
+    private JLabel gjsdlsjkhjksdhkjsdjkshdgsdfhjkg;
+    final ArrayList<ChangeListener> listeners = new ArrayList<>();
+
+    public JPanel getJklsdhgisdhgsfdkhjgsfg() {return jklsdhgisdhgsfdkhjgsfg;}
+
+}
