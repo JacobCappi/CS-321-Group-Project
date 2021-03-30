5d9092aed94223f4314df2421914a60b716c23d9
gdors <gcd0003@uah.edu>
2021-03-30 09:44:10 -0500

    created Manager.java and working on refining UserList.java

---
+++ src/Manager.java
@@ -0,0 +1,44 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Manager
+{
+    private final JSONParser parser = new JSONParser();
+
+    public boolean loginUser (User insertUser) throws IOException, ParseException {
+        Object obj = parser.parse(new FileReader(String.valueOf(Paths.get("test.json")))); // Object that will parse the JSON file for its information
+        JSONArray jsonArray = (JSONArray) obj;
+
+        for (Object o : jsonArray) {
+            JSONObject currentOBJ = (JSONObject) o;
+            if (insertUser.getName().equals((String) currentOBJ.get("username")) && insertUser.getPassword().equals((String) currentOBJ.get("password"))) {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+    public boolean addUser(String inputUsername, String inputPassword) throws IOException, ParseException {
+        Object obj = parser.parse(new FileReader(String.valueOf(Paths.get("test.json")))); // Object that will parse the JSON file for its information
+        JSONArray addUserJSONArray = (JSONArray) obj;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", inputUsername);
+        jsonObject.put("password",inputPassword);
+        addUserJSONArray.add(jsonObject);
+        FileWriter jsonFileWriter = new FileWriter(String.valueOf(Paths.get("test.json")));
+        jsonFileWriter.write(addUserJSONArray.toJSONString());
+        jsonFileWriter.flush();
+        jsonFileWriter.close();
+        return true;
+
+
+    }
+}
--- src/UserList.java
+++ src/UserList.java
@@ -8,38 +8,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class UserList {
-   private final JSONParser parser = new JSONParser();
+    ArrayList<User> listOfUsers = new ArrayList<>();
+    int length;
+  public UserList(){ length = 0;}
 
-    public boolean loginUser (User insertUser) throws IOException, ParseException {
-        Object obj = parser.parse(new FileReader(String.valueOf(Paths.get("test.json")))); // Object that will parse the JSON file for its information
-        JSONArray jsonArray = (JSONArray) obj;
-
-        for (Object o : jsonArray) {
-            JSONObject currentOBJ = (JSONObject) o;
-            if (insertUser.getName().equals((String) currentOBJ.get("username")) && insertUser.getPassword().equals((String) currentOBJ.get("password"))) {
-                return true;
-            }
-        }
-        return false;
+  public void addUser(User user){
+      listOfUsers.add(user);
+     length++;
+  }
 
 
-    }
-    public boolean addUser(String inputUsername, String inputPassword) throws IOException, ParseException {
-        Object obj = parser.parse(new FileReader(String.valueOf(Paths.get("test.json")))); // Object that will parse the JSON file for its information
-        JSONArray addUserJSONArray = (JSONArray) obj;
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("username", inputUsername);
-        jsonObject.put("password",inputPassword);
-        addUserJSONArray.add(jsonObject);
-        FileWriter jsonFileWriter = new FileWriter(String.valueOf(Paths.get("test.json")));
-        jsonFileWriter.write(addUserJSONArray.toJSONString());
-        jsonFileWriter.flush();
-        jsonFileWriter.close();
-        return true;
-
-
-    }
 
 }
